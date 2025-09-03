b4d999e15460d3ed460ccf7407a3cd64a21309bf
Ethan <ethankremerdojc@gmail.com>
2025-09-03 13:47:06 -0600

    remove post reqs

--- guide/views.py
+++ guide/views.py
@@ -138,16 +138,15 @@ def guide_page(request, book, chapter, *args, **kwargs):
     })
 
 def get_chapter_info(request, *args, **kwargs):
-     if request.method == "POST":
-        data = json.loads(request.body)
-        book = get_cleaned_alpha_text(data.get("book"))
-        chapter = data.get("chapter")
-        version = data.get("version")
+    data = json.loads(request.body)
+    book = get_cleaned_alpha_text(data.get("book"))
+    chapter = data.get("chapter")
+    version = data.get("version")
 
-        chapter_info = get_chapter_bible_hub(book_name=book, chapter_num=chapter)
-        chapter_html = get_chapter_html(book=book, chapter=chapter, version=version, chapter_info=chapter_info)
+    chapter_info = get_chapter_bible_hub(book_name=book, chapter_num=chapter)
+    chapter_html = get_chapter_html(book=book, chapter=chapter, version=version, chapter_info=chapter_info)
 
-        return JsonResponse({"chapterInfo": chapter_info, "chapterText": chapter_html})
+    return JsonResponse({"chapterInfo": chapter_info, "chapterText": chapter_html})
 
 def get_word_info_from_verse(verse_info, word):
     cleaned_word = get_cleaned_alpha_text(word)
@@ -204,14 +203,13 @@ def get_word_info_from_verse(verse_info, word):
             print(new_candidates)
 
 def get_word_info(request, *args, **kwargs):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        word = get_cleaned_alpha_text(data.get("word"))
-        verse = data.get("verse").replace("verse", "") # ie. verse14
+    data = json.loads(request.body)
+    word = get_cleaned_alpha_text(data.get("word"))
+    verse = data.get("verse").replace("verse", "") # ie. verse14
 
-        chapter_info = get_chapter_bible_hub(book_name="john", chapter_num=3)
+    chapter_info = get_chapter_bible_hub(book_name="john", chapter_num=3)
 
-        verse_info = chapter_info[verse]
-        word_info = get_word_info_from_verse(verse_info, word)
+    verse_info = chapter_info[verse]
+    word_info = get_word_info_from_verse(verse_info, word)
 
-        return JsonResponse({"result": "success", "word_info": word_info})
+    return JsonResponse({"result": "success", "word_info": word_info})
